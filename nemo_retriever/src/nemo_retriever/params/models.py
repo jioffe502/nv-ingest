4d37cec2b34b3e5c4afbff6c86c839a53fa2d575
jioffe502 <jioffe@nvidia.com>
2026-04-21 20:02:58 +0000

    Set default embed batch size to 32

--- nemo_retriever/src/nemo_retriever/params/models.py
+++ nemo_retriever/src/nemo_retriever/params/models.py
@@ -149,7 +149,7 @@ class BatchTuningParams(_ParamsModel):
     page_elements_cpus_per_actor: float = 1
     ocr_cpus_per_actor: float = 1
     embed_workers: Optional[int] = None
-    embed_batch_size: int = 256
+    embed_batch_size: int = 32
     embed_cpus_per_actor: float = 1
     gpu_page_elements: Optional[float] = None
     gpu_ocr: Optional[float] = None
